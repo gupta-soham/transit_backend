3ab582b0dd75e195d7ca9b932eddb11c13014e98
Abhit <121966052+abhitsahu@users.noreply.github.com>
2025-03-24 12:55:30 +0530

    Update rateLimiter.ts

--- src/middlewares/rateLimiter.ts
+++ src/middlewares/rateLimiter.ts
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 10,
+  max: 100,
   message: { message: "Too many requests from this IP, please try again later." },
   headers: true, // Send RateLimit headers
 });
